c37df353d37522c402f33991e66749a962a8d678
guillaume <guillaumedefaye9@gmail.com>
2022-05-24 15:04:19 +0200

    fixing issue

--- script/kube_install.sh
+++ script/kube_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg |  apt-key add -
 
 echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt install kubelet kubeadm kubectl -y
+ apt install kubelet kubeadm kubectl -y
