45f0db8527097b73b527193fe9303291440579d7
guillaume <guillaumedefaye9@gmail.com>
2022-05-24 14:09:36 +0200

    fixing

--- script/docker_install.sh
+++ script/docker_install.sh
@@ -4,7 +4,7 @@ echo Docker Install
 
 #Disable swap memory
 
-sudo swapoff -a
+#swapoff -a
 
 #  Package update 
 
@@ -16,17 +16,17 @@ apt update -y
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 
 # Add GPG key for the official Docker repository
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -
 
 # Add the docker repository to APT sources
 
  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update the package again
 
@@ -34,7 +34,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 
 # Install from the docker repo
 
- sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 # Install docker 
 
@@ -42,31 +42,32 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 
 # Troubleshooting docker group 
 
- usermod -aG docker ${USER}
+ sudo usermod -aG docker $USER
+
 
 # Check if Docker is correctly installed
 
  systemctl status docker
 
 #Setup the Docker daemon to use systemd as the cgroup driver
-
-sudo bash -c 'cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF'
+#
+ #bash -c 'cat > /etc/docker/daemon.json <<EOF
+# {
+ # "exec-opts": ["native.cgroupdriver=systemd"],
+#  "log-driver": "json-file",
+ ## "log-opts": {
+#    "max-size": "100m"
+#  },
+#  "storage-driver": "overlay2"
+#}
+#E#OF'
 
 
 #Create a systemd directory for Docker
 
-sudo mkdir -p /etc/systemd/system/docker.service.d
+#sudo mkdir -p /etc/systemd/system/docker.service.d
 
 #Restart Docker
 
-sudo systemctl daemon-reload
-sudo systemctl restart docker
+#sudo systemctl daemon-reload
+#sudo systemctl restart docker
